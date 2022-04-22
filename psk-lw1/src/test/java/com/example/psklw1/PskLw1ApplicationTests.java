d4b64715cdee431f4bafa75a845f9971b966110c
Rytis Cepulis <cepRytis@gmail.com>
2022-04-22 18:05:12 +0300

    dependencies

---
+++ psk-lw1/src/test/java/com/example/psklw1/PskLw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psklw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PskLw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
